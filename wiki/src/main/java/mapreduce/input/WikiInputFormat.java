e91ce3308ff59201035eede0fcb9d4326236c7fb
2338067g <2338067g@quickstart.cloudera>
2018-02-07 10:20:07 -0800

    ajustes sobre clase WikiInputFormat

--- wiki/src/main/java/mapreduce/input/WikiInputFormat.java
+++ wiki/src/main/java/mapreduce/input/WikiInputFormat.java
@@ -5,7 +5,10 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 
 import mapreduce.datatypes.WikiInputValue;
-
+/**
+ * @author 2338066f ANTONIO FEREGRINO BOLANOS
+ * @author 2338067g HOMERO GARCIA MERINO
+ */
 public class WikiInputFormat extends FileInputFormat<Text, WikiInputValue>{
 	public RecordReader<Text, WikiInputValue> createRecordReader(InputSplit split, TaskAttemptContext context){
 		return new WikiMultiRecordReader();
