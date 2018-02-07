cd5a851e868ed17e902e4473ea01d8346df9d87f
2338067g <2338067g@quickstart.cloudera>
2018-02-07 10:11:55 -0800

    ajuste matricula Toño

--- wiki/src/main/java/mapreduce/WikiInputFormat.java
+++ wiki/src/main/java/mapreduce/WikiInputFormat.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 /**
- * @author XXXXXXXf ANTONIO FEREGRINO BOLANOS
+ * @author 2338066f ANTONIO FEREGRINO BOLANOS
  * @author 2338067g HOMERO GARCIA MERINO
  */
 public class WikiInputFormat extends FileInputFormat<Text, Text>{
