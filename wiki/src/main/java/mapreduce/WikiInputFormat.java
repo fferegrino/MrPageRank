4e3d455a55ef672091ab19e4fc0ee306707ff8cc
2338067g <2338067g@quickstart.cloudera>
2018-02-07 09:06:54 -0800

    prueba de commit desde Eclipse-workspace

--- wiki/src/main/java/mapreduce/WikiInputFormat.java
+++ wiki/src/main/java/mapreduce/WikiInputFormat.java
@@ -3,7 +3,10 @@ package mapreduce;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
-
+/**
+ * @author XXXXXXXf ANTONIO FEREGRINO BOLANOS
+ * @author 2338067g HOMERO GARCIA MERINO
+ */
 public class WikiInputFormat extends FileInputFormat<Text, Text>{
 	public RecordReader<Text, Text> createRecordReader(InputSplit split, TaskAttemptContext context){
 		return new WikiMultiRecordReader();
