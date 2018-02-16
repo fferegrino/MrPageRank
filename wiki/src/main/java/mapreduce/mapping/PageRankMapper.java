e923c3014ded25e3564fdd05fa8ec076c466097f
Homero Merino <2338067g@student.gla.ac.uk>
2018-02-16 18:31:06 +0000

    actualización de comentarios que faltaban

--- wiki/src/main/java/mapreduce/mapping/PageRankMapper.java
+++ wiki/src/main/java/mapreduce/mapping/PageRankMapper.java
@@ -6,15 +6,27 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-
+/**
+ * PageRankMapper class that creates the in-links list for page Y based on the out-links 
+ * list for page X by replacing the Key (article's name) of the Parent article with
+ * the Referenced or "Child" article, and setting the Parent's current PageRank.
+ * @author 2338066f ANTONIO FEREGRINO BOLANOS
+ * @author 2338067g HOMERO GARCIA MERINO
+ */
 public class PageRankMapper extends Mapper<LongWritable, Text, Text, WikiIntermediatePageRankValue> {
-
+    /**
+     * Map method that generates the in-links list for each out-link element referenced by an Article.
+     * @param key     value for Parent article.
+     * @param value   with Parent article detail.
+     * @param context object contains configuration data to interact with Hadoop's
+     *                environment.
+     */
     @Override
     protected void map(LongWritable key, Text line,
                        Mapper<LongWritable, Text, Text, WikiIntermediatePageRankValue>.Context context)
             throws IOException, InterruptedException {
 
-        // En parent value viene toda la línea en este formato: parent\tPR|#outlinks|out1 out2 out3
+        // In parent value the hole line has this format: parent\tPR|#outlinks|out1 out2 out3
         String[] split = line.toString().split("\t");
 
         String parent = split[0].trim();
@@ -23,12 +35,13 @@ public class PageRankMapper extends Mapper<LongWritable, Text, Text, WikiInterme
 
         float parentPageRank = Float.parseFloat(values[0]);
         int parentNumberOfOutlinks = Integer.parseInt(values[1]);
-
+        // Create intermediate value for looping process
         WikiIntermediatePageRankValue intermediateValue;
         Text intermediateKey;
-
+        // Iterate over Article's out-links only if it has out-links
         if (values.length > 2) {
             String[] parentOutlinks = values[2].split(" ");
+            // Loop that performs in-links generation
             for (int i = 0; i < parentNumberOfOutlinks; i++) {
                 String currentOutlink = parentOutlinks[i];
 
@@ -38,13 +51,12 @@ public class PageRankMapper extends Mapper<LongWritable, Text, Text, WikiInterme
                 intermediateValue.setPageRank(parentPageRank);
                 intermediateValue.setParent(parent);
                 intermediateValue.setParentOutlinksNumber(parentNumberOfOutlinks);
-                // No se necesitan, otra vez
-
+                // Write in-link Referenced Key with Parent (voter) details
                 context.write(intermediateKey, intermediateValue);
             }
 
         }
-        // Send again the parent with the list of outlinks:
+        // Create new Parent object with original information (recovery of state in Reducer phase)
         intermediateKey = new Text(parent);
 
         intermediateValue = new WikiIntermediatePageRankValue();
@@ -54,7 +66,7 @@ public class PageRankMapper extends Mapper<LongWritable, Text, Text, WikiInterme
             intermediateValue.setOutlinks(values[2]);
         }
         intermediateValue.setParentOutlinksNumber(parentNumberOfOutlinks);
-
+        // Write the parent with the original out-links details
         context.write(intermediateKey, intermediateValue);
     }
 
--- wiki/src/main/java/mapreduce/reducing/PageRankReducer.java
+++ wiki/src/main/java/mapreduce/reducing/PageRankReducer.java
@@ -7,51 +7,70 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 import java.util.Iterator;
-
+/**
+ * PageRankReducer class performs the PageRank calculation, by taking a list of 'WikiIntermediatePageRankValue'
+ * objects, allowing the calculation of a given page over a single loop where the processed records must meet 
+ * the following conditions:
+ * - Parent field's value must be different to the key value
+ * - Not having out-links specified in the "out-links" field 
+ * @author 2338066f ANTONIO FEREGRINO BOLANOS
+ * @author 2338067g HOMERO GARCIA MERINO
+ */
 public class PageRankReducer extends Reducer<Text, WikiIntermediatePageRankValue, Text, WikiInOutPageRankValue> {
-
+	// Constant for calculation
     final float d = 0.85f;
-
+    
+    /**
+     * Reduce method that performs the PageRank calculation.
+     * @param inKey   referenced article's name.
+     * @param value   as defined by the WikiIntermediatePageRankValue class.
+     * @param context object contains configuration data to interact with Hadoop's
+     *                environment and defines OutputFormat types for key-value.
+     */
     @Override
     protected void reduce(Text inKey, Iterable<WikiIntermediatePageRankValue> inValues,
                           Reducer<Text, WikiIntermediatePageRankValue, Text, WikiInOutPageRankValue>.Context context)
             throws IOException, InterruptedException {
-
+    	// Variable that stores new calculated value
         float newPageRank;
         float vote = 0;
+        // List of out-links
         String outlinks = null;
+        // Number of out-links elements (pre-computed)
         int outlinksNumber = 0;
 
         String key = inKey.toString();
-
+        // Variable for storing input object
         WikiIntermediatePageRankValue value;
+        // PageRank calculation
         for (Iterator<WikiIntermediatePageRankValue> it = inValues.iterator(); it.hasNext(); ) {
             value = it.next();
+            // validate if calculation should be made, or is the Parent record
             if (value.getParent().equalsIgnoreCase(key) && value.getOutlinks() != null && !value.getOutlinks().equals("")) {
                 outlinks = value.getOutlinks();
                 outlinksNumber = value.getParentOutlinksNumber();
                 continue;
             }
-
-
+            // No parent out-links, means the voting result is zero
             if (value.getParentOutlinksNumber() == 0) {
                 continue;
             }
+            // Voting section by each page
             vote += value.getPageRank() / value.getParentOutlinksNumber();
         }
+        // Final PageRank calculation
         newPageRank = (1 - d) + (d * vote);
-
+        // Replace null out-links list for empty space to avoid errors
         if (outlinks == null) {
             outlinks = "";
         }
-
+        // Create output object with new results
         Text outKey = new Text(key);
         WikiInOutPageRankValue outValue = new WikiInOutPageRankValue();
         outValue.setOutlinks(outlinks);
         outValue.setPageRank(newPageRank);
         outValue.setOutlinksNumber(outlinksNumber);
-
-
+        // Write processed data to the context data flow
         context.write(outKey, outValue);
     }
 
