34485f997831fa6ed35c7c8ade3eab16c917056d
Homero Merino <2338067g@student.gla.ac.uk>
2018-03-11 23:27:49 +0000

    documentación final del objeto

--- wiki-spark/scala/src/main/scala/pageRank/Improved.scala
+++ wiki-spark/scala/src/main/scala/pageRank/Improved.scala
@@ -1,7 +1,11 @@
 package pageRank
 
+import pageRank.dataTypes.WikipediaRevision
+import pageRank.dataTypes.WikipediaRevision
+
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.fs.FileSystem
@@ -9,19 +13,37 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
 import org.apache.hadoop.io.LongWritable
 import org.apache.hadoop.io.Text
 
-import pageRank.dataTypes.WikipediaRevision
-import pageRank.dataTypes.WikipediaRevision
-
-import org.apache.spark.api.java.JavaPairRDD
-import java.util.ArrayList
-import utils.ISO8601
-import java.util.Date
 import scala.collection.immutable.Set
-import org.apache.spark.rdd.RDD
+
+import java.util.ArrayList
+import java.util.Date
+import utils.ISO8601
+
+
+/**
+ * Main object for PageRank's calculation of a given set using Apache Hadoop's ecosystem.
+ * The PageRank is an algorithm used to establish web-pages relevance or importance within
+ * a web-site or the Internet.
+ *
+ * @author 2338066f ANTONIO FEREGRINO BOLANOS
+ * @author 2338067g HOMERO GARCIA MERINO
+ */
 
 object Improved {
-
+ 
+  /**
+   * Main method responsible for orchestrating and managing the PageRank's calculation
+   * scheduling Spark jobs in N iterations as requested by the client.
+   *
+   * @param args is a string array that contains the full set of command-line parameters.
+   *   @param[0] input file path, e.g. Wikipedia edit history file.
+   *   @param[1] output directory path (it can exist or not).
+   *   @param[2] number of iterations for PageRank's calculation.
+   *   @param[3] ISO8601-formatted date for which the PageRank scores will be computed.
+   */
   def main(args: Array[String]) {
+    // arguments validation and usage display in case of incorrect input of parameters
+    // Iterations (args(2)) and ISO8601-formatted date (args(3)) arguments are optional
     if (args.length <  3) {
       System.err.println("MrPageRankII usage: PageRank <String input_filepath> <String output_filepath> <Int iterations> <Datetime YYYY-MM-DD'T'HH:mm:ss'Z' format>")
       System.exit(1)
@@ -30,9 +52,8 @@ object Improved {
     // arguments handling
     val inputFile = args(0)
     val outputFile = new Path(args(1))
-    val iterations = iterToInt(args(2))
-    val timeLimit = if (args.length == 4) datetimeToISO8601(args(3)) else new Date().getTime
-    
+    val iterations = iterToInt(args(2)) // A default value is assigned if no value is present or the format is incorrect
+    val timeLimit = if (args.length == 4) datetimeToISO8601(args(3)) else new Date().getTime // A default value is assigned if no value is present or the format is incorrect
     
     // create configuration object and register application
     val conf = new SparkConf().setAppName("Mighty-WikiPageRank_II")
@@ -48,13 +69,13 @@ object Improved {
     // Set input format record delimiter for Wikipedia's HDFS files
     sc.hadoopConfiguration.set("textinputformat.record.delimiter", "\n\n")  
 
-    // All revisions, filtered initially by date
+    // Data gathering from HDFS filesystem
     val revisions: RDD[WikipediaRevision] = sc.newAPIHadoopFile(inputFile, classOf[TextInputFormat], classOf[LongWritable], classOf[Text])
       .values
-      .map(textToRevision)
-      .filter(r => r.revisionTime <= timeLimit)
+      .map(textToRevision) // selection and cleansing processes
+      .filter(r => r.revisionTime <= timeLimit) // All revisions, filtered initially by date
 
-    // Second, and final filtering step
+    // Second, and final filtering step (group by to get the last date of the filtered selection)
     val singleRevisions: RDD[WikipediaRevision] = revisions.groupBy(revision => revision.articleId)
       .mapValues(revisions => {
         var articleId = ""
@@ -69,22 +90,24 @@ object Improved {
             outlinks = if (revision.outlinks == null) "" else revision.outlinks
           }
         }
-
+        // outlinks cleansing and Parent article Id assigned at the end of the string
         val cleanLinks = outlinks.split("\\s+").toSet - "" + articleId
+        // wrapping data into WikipediaRevision class
         new WikipediaRevision(articleId, 0, cleanLinks.mkString(" "))
       }).values
 
-    // Pairs in the form: (Parent, outlink)
+    // Pairs in the form: (Parent article Id, outlinks)
     val links: RDD[(String, Array[String])] = singleRevisions
       .map(rev => (rev.articleId, rev.outlinks.split("\\s"))).cache()
 
-    // Pairs in the form: (Parent, PageRank score)
+    // Pairs in the form: (Parent article Id, PageRank score)
     var ranks: RDD[(String, Double)] = links.mapValues(rev => 1.0)
     // Number of partitions asigned to ranks
     var ranks_par = ranks.partitions.size
-
+    
+    // PageRank's iterative calculation
     for (i <- 1 to iterations) {
-      // Pairs in the form: (Children, PageRank contribution from parent)
+      // Pairs in the form: (Children, PageRank contribution from Parent article Id)
       val contribs: RDD[(String, Double)] = links.join(ranks)
         .flatMap((values) => {
           val(parentArticle, info) = values
@@ -99,12 +122,12 @@ object Improved {
           }
           res
         })
+      // PageRank calculation - repartition is performed to provide more resources for calculation stage
       ranks = contribs.reduceByKey((a, b) => a + b).mapValues(v => 0.15 + v * 0.85).repartition(ranks_par*3).setName("Rankings_RDD")
     }
-    
+    // Sorting in descending score order and delivery - write results to HDFS output path
     ranks.sortBy(f=>f._2, false,ranks_par*2).map(prs => prs._1+ " " + prs._2).setName("Sorting_RDD").saveAsTextFile(args(1))
     //ranks.sortBy(f=>f._2, false,40).map(prs => prs._1+ " " + prs._2).saveAsTextFile(args(1))
-    //ranks.map(prs => prs._1 + " " + prs._2).saveAsTextFile(args(1))
 
     // Close the SparkContextobject, releasing its resources.
     sc.stop()
@@ -145,16 +168,21 @@ object Improved {
     }
   }
   
+  /**
+     * Selection and cleansing process for gathered data.  
+     * @param text Text textinputformat data.
+     * @return WikipediaRevision wrapping object.
+     */ 
   def textToRevision(text: Text) : WikipediaRevision  = {
-        val lines = text.toString().split("\n")
-        val revision = lines(0).split(" ")
-        val revisionId = revision(2).trim().toLong
-        val articleId = revision(3).trim()
-        val revisionTime = ISO8601.toTimeMS(revision(4).trim())
-        val mainArray = lines(3).split(" ", 2)
+        val lines = text.toString().split("\n") //identify attributes delimited by lines break
+        val revision = lines(0).split(" ") //split revision's attribute contents with spaces
+        val revisionId = revision(2).trim().toLong //get Revision Id from revision's attribute
+        val articleId = revision(3).trim() //get Parent article Id from revision's attribute
+        val revisionTime = ISO8601.toTimeMS(revision(4).trim()) //get Revision time from revision's attribute
+        val mainArray = lines(3).split(" ", 2) //split main's attribute contents with spaces
         var outlinks: String = null
         if (mainArray.length == 2) {
-          outlinks = mainArray(1)
+          outlinks = mainArray(1) //get Out-links from main's attribute
         }
         return new WikipediaRevision(articleId, revisionId, outlinks, revisionTime)
   }
